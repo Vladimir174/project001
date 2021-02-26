fd6ef98fbc7d487fe7688aa12b0c1a5fc26d4acc
Vladimir <magvaivladimir@gmail.com>
2021-02-26 14:27:18 +0300

    first commit

---
+++ 01/js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+/* alert('Hello'); */
+
+/* const result = confirm("Are you here?"); */
+/* console.log(result); */
+
+/* const answer = +prompt("Вам есть 18?", "18");
+console.log(answer + 5); */
+
+/* const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers); */
+
+/* const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`); */
+
+console.log(6 + "7");
+
+let incr = 10, decro = 10;
+
+/* incr++;
+decro--; */
+console.log(++incr);
+console.log(--decro);
+
+console.log(5%2); 
+
+console.log(2*4 === 8); //строгое равенство ===
+
+
+
+const isChecked = true,
+        isClose = false;
+
+        console.log(isChecked || isClose); // или то или то true
+        console.log(isChecked && isClose); //и это и это верно если оба true. оператор отрецаня !
+
+
+
