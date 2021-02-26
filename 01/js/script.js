6094238c6565d02dbcafcc11a4b089cfa2075e14
Vladimir <magvaivladimir@gmail.com>
2021-02-26 17:43:22 +0300

    The first app in web.

--- 01/js/script.js
+++ 01/js/script.js
@@ -23,7 +23,22 @@
 
 'use strict';
 
-const personalMovieDB {
-    count: 
-    movies: 
-}
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actor: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних прсмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних прсмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
